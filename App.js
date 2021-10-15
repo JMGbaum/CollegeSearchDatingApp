8a4da9b86afcb99ad0ecdf8c3cdaa3dfc77f1bef
JMGbaum <32182454+JMGbaum@users.noreply.github.com>
2021-10-14 23:47:15 -0400

    Simplify main file

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
+import {NavBar} from './components'
 import React from 'react';
-import { SafeAreaView, View, Button } from 'react-native';
+import { SafeAreaView, View } from 'react-native';
 import { styles } from './Styles';
 import { Stack } from "./screens/Stack";
 
@@ -8,14 +9,8 @@ export default function App() {
   return (
     <View style={{flex:1, backgroundColor:"#000000"}}>
       <SafeAreaView style={styles.container}>
-        <View>
-          <Stack/>
-        </View>
-        <View style={styles.navbar}>
-          <Button id="likes" title="Likes"></Button>
-          <Button id="stack" title="Stack"></Button>
-          <Button id="settings" title="Settings"></Button>
-        </View>
+        <Stack/>
+        <NavBar/>
       </SafeAreaView>
     </View>
   );

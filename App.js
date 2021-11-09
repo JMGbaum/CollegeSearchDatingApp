fa14fd7753c41e391a00879e97badc8837cb95b1
Rebecca Zipper <63117945+beckystar4@users.noreply.github.com>
2021-11-08 21:52:56 -0500

    nav bar for stack, likes, and settings. Nav bar changes depending on what screen you are on.

--- App.js
+++ App.js
@@ -1,16 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import {NavBar} from './components'
 import React from 'react';
-import { SafeAreaView, View, StyleSheet } from 'react-native';
-import { Stack } from "./screens/Stack";
+import { SafeAreaView} from 'react-native';
 
 import Question from './Navigation/Questions';
-//<NavBar/>
 
 export default function App() {
   return (
-    <SafeAreaView style={{flex: 1}}>
+    <SafeAreaView style={{flex:1}}>
       <Question/>
     </SafeAreaView>
   );
 }
+
--- Navigation/Questions.js
+++ Navigation/Questions.js
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { SafeAreaView, View, Button,Dimensions, StyleSheet} from 'react-native';
-import { NavBar } from '../components/NavBar';
+import { SafeAreaView, View, Button, StyleSheet,Dimensions, Text} from 'react-native';
+
+
 import { Stack } from "../screens/Stack";
 import { Logs } from "../screens/Login";
+import { Settings_Screen } from "../screens/Settings";
 import { Register_Screen } from "../screens/Register";
-
-
 import {Q1} from "../Questions/Question1";
 import {Q2} from "../Questions/Question2";
 import {Q3} from "../Questions/Question3";
@@ -155,44 +155,26 @@ const Question5Screen = ({navigation}) => {
 const RealApp = ({navigation}) => {
   return(
     <View style={{flex:1, backgroundColor:"#000000"}}>
-      <SafeAreaView style = {new_styles.container}>
+      <SafeAreaView style={new_styles.container}>
         <View>
           <Stack />
           <StatusBar style="auto" />
-          <NavBar/>
+        </View>
+        <View style={new_styles.navbar}>
+          <Button 
+              title="Settings"
+              onPress={() =>
+                navigation.navigate('Settings')
+              }
+          />
+          <Button title="Likes"
+              onPress={() =>
+                navigation.navigate('Likes')
+              }></Button>
+          
         </View>
       </SafeAreaView>
     </View>
-
-  );
-};
-/*
-<View style={new_styles.navbar}>
-          <Button 
-            id="likes" 
-            title="Likes"
-            onPress={() =>
-              navigation.navigate('Likes')
-            }
-          >
-          </Button>
-          <Button id="settings" title="Settings" onPress={() =>
-              navigation.navigate('Settings')
-            }></Button>
-        </View>
-
-const Likes = ({navigation}) => {
-  return(
-    <SafeAreaView>
-      <Text> These are likes </Text>
-      <Button 
-        title="Done"
-        color='black'
-        onPress={() =>
-          navigation.navigate('Login')
-        }
-      />
-    </SafeAreaView>
 
   );
 };
@@ -200,23 +182,48 @@ const Likes = ({navigation}) => {
 const Settings = ({navigation}) => {
   return(
     <SafeAreaView>
-      <Text> These are settings </Text>
-      <Button 
-        title="Done"
-        color='black'
-        onPress={() =>
-          navigation.navigate('Login')
-        }
-      />
+      <Settings_Screen/>
+      <View style={new_styles.other_navbar}>
+          <Button 
+              title="Stack"
+              onPress={() =>
+                navigation.navigate('Real App')
+              }
+          />
+          <Button title="Likes"
+              onPress={() =>
+                navigation.navigate('Likes')
+              }></Button>
+          
+      </View>
     </SafeAreaView>
+
 
   );
 };
-<QuestionStack.Screen name="Likes" component={Likes} />
-<QuestionStack.Screen name="Settings" component={Settings} />
-*/
 
+const Likes = ({navigation}) => {
+  return(
+    <SafeAreaView>
+      <Text style = {new_styles.title_text}> Welcome to your likes!</Text>
+      <View style={new_styles.other_navbar}>
+          <Button 
+              title="Stack"
+              onPress={() =>
+                navigation.navigate('Real App')
+              }
+          />
+          <Button title="Settings"
+              onPress={() =>
+                navigation.navigate('Settings')
+              }></Button>
+          
+      </View>
 
+    </SafeAreaView>
+    
+  );
+};
 export function Question() {
   return (
     <NavigationContainer>
@@ -237,8 +244,10 @@ export function Question() {
         <QuestionStack.Screen name="Question 4" component={Question4Screen} />
         <QuestionStack.Screen name="Question 5" component={Question5Screen} />
 
-        
-        
+        <QuestionStack.Screen name="Settings" component={Settings}
+        options={{ headerBackVisible: false}}/>
+        <QuestionStack.Screen name="Likes" component={Likes}
+        options={{ headerBackVisible: false}}/>
         
         
         
@@ -250,6 +259,12 @@ export function Question() {
 export default Question;
 
 const new_styles = StyleSheet.create({
+  text:{
+    fontSize: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
   stylize: {
     flex: 1, //flexible
     backgroundColor: '#9BD1D1',
@@ -273,12 +288,6 @@ const new_styles = StyleSheet.create({
     alignItems: 'center',
   },
 
-  container: {
-    backgroundColor: '#000',
-    alignItems: 'center',
-    justifyContent: 'center',
-    flex: 1,
-  },
   navbar: {
     width: Dimensions.get("screen").width,
     height: Dimensions.get("screen").height*0.1,
@@ -286,10 +295,31 @@ const new_styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "center",
     alignItems:"center",
-    top: Dimensions.get("screen").height*0.9,
+    top: Dimensions.get("screen").height*0.84,
     zIndex: 10,
     position:"absolute",
   },
+
+  other_navbar: {
+    width: Dimensions.get("screen").width,
+    height: Dimensions.get("screen").height*0.1,
+    backgroundColor: "#b1b1b1",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems:"center",
+    top: Dimensions.get("screen").height*0.78,
+    zIndex: 10,
+    position:"absolute",
+  },
+
+  container: {
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+  },
+
+
   
 });
 
